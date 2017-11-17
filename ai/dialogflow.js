0a1c3e5d9f59390d7d081f22c6d1eca75687ff02
canvip <canvip2011@gmail.com>
2017-11-17 22:09:11 +0000

    Checkpoint

--- ai/dialogflow.js
+++ ai/dialogflow.js
@@ -2,7 +2,7 @@ var apiai = require('apiai');
 
 var app = apiai("a3c53c4bdfd24f8d87641c80cc5eaebf");
 
-var request = app.textRequest(' بك اهلا', {
+var request = app.textRequest(' hi im ahmed , you ', {
     sessionId: 'newagent-3f1fc'
 });
 
