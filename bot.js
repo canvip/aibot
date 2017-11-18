7bcdb545bfddebbf878592c84754cbcc1dd6b290
canvip <canvip2011@gmail.com>
2017-11-18 15:16:37 +0000

    Checkpoint

--- bot.js
+++ bot.js
@@ -94,7 +94,7 @@ app.set('views', path.join(__dirname, 'views'));//canvip
 app.set('view engine', 'ejs');
 //app.use('/public', express.static('/public'))
 //app.set('views/scripts/app.js', path.join(__dirname, 'views/scripts/app.js'));
-app.use('/styles', express.static(path.join(__dirname, 'public')))
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')))
 app.use('/', routes);
 
 //const funz = require('./funz');
