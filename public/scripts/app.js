6e2452a2c68d40ad27437669d46b779850d7496f
canvip <canvip2011@gmail.com>
2017-11-19 00:22:00 +0000

    Checkpoint

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -365,6 +365,6 @@
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+             .then(function(reg) { console.log('Service Worker Registered'); });
   }
 })();
